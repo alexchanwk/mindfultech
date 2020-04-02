b1eeeb0d1ca3a9a9b56fdf02dca54d79706e893b
Alex Chan <alexchanwk@gmail.com>
2020-04-02 23:03:00 +0800

    add survey plugin

---
+++ static/app/mindfultech-survey/precache-manifest.b966e8682fdbfad8e9e8fcdd3c41de84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b16edfcf1ebdb73c039faf37eedb67a",
+    "url": "/app/mindfultech-survey/index.html"
+  },
+  {
+    "revision": "082c41f32d08202374a6",
+    "url": "/app/mindfultech-survey/static/css/main.1bbef3c2.chunk.css"
+  },
+  {
+    "revision": "16bf926e07cc76a5de24",
+    "url": "/app/mindfultech-survey/static/js/2.a11e753e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/mindfultech-survey/static/js/2.a11e753e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082c41f32d08202374a6",
+    "url": "/app/mindfultech-survey/static/js/main.39538abc.chunk.js"
+  },
+  {
+    "revision": "37c5d2097826a32640bb",
+    "url": "/app/mindfultech-survey/static/js/runtime-main.4cd43547.js"
+  }
+]);
